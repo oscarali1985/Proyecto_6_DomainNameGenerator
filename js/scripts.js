5e1995368e22f34e2d6c6623b24c8defa6527a82
oscarali1985 <oscarali1985@hotmail.com>
2020-07-16 23:17:49 -0400

    Proyecto Actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
+/*
+
+thegreatjogger.com 
+thegreatracoon.com
+ ourgreatjogger.com
+ ourgreatracoon.com 
+thebigjogger.com
+thebigracoon.com
+ourbigjogger.com
+ourbigracoon.com
+
+ourbigracoon.com ourbigracoon.com ourbigracoon.com ourbigracoon.com 
+scripts.js:51 thegreatjogger.com thegreatjogger.com thegreatjogger.com thegreatjogger.com 
+*/
+
 // Add Your Scripts here
+
+
+
+var pronoun = ['the','our']; 
+var adj = ['great', 'big' ]; 
+var noun = ['jogger','racoon'];
+resutldomain =""
+
+for (let pron of pronoun) {
+    for (let ad of adj) {
+        for( let no of noun){
+            resutldomain +=`${pron}${ad}${no}.com \n`
+        }
+    }
+}
+
+
+document.getElementById("domain").innerHTML = resutldomain;
+console.log(resutldomain)
